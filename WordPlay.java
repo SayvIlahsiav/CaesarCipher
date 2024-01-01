d3d8a016d198db9bc556d5da38e8e989ad41e022
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-01-01 18:34:07 +0530

    Implemented emphasize method

--- WordPlay.java
+++ WordPlay.java
@@ -27,6 +27,18 @@ public class WordPlay {
         return replaced.toString();
     }
     
+    public String emphasize(String phrase, char ch) {
+        StringBuilder emphasized = new StringBuilder();
+        for (int i = 0; i < phrase.length(); i++) {
+            if (Character.toLowerCase(phrase.charAt(i)) == Character.toLowerCase(ch)) {
+                if (i%2 == 0) emphasized.append('*');
+                else emphasized.append('+');
+            }
+            else emphasized.append(phrase.charAt(i));
+        }
+        return emphasized.toString();
+    }
+    
     public void testIsVowel() {
         System.out.println(isVowel('F'));
         System.out.println(isVowel('f'));
@@ -39,9 +51,16 @@ public class WordPlay {
         System.out.println(replaceVowels("Hello World", 'y'));
     }
     
+    public void testEmphasize() {
+        System.out.println(emphasize("Hello World", 'o'));
+        System.out.println(emphasize("Hello World", 'l'));
+        System.out.println(emphasize("dna ctgaaactga", 'a'));
+        System.out.println(emphasize("Mary Bella Abracadabra", 'a'));
+    }
+    
     public static void main(String[] args) {
         WordPlay wp = new WordPlay();
-        wp.testReplaceVowels();
+        wp.testEmphasize();
     }
 
 }
