7addfbc95aae6fbf0382db205532357a3f35044a
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-01-01 15:09:01 +0530

    Implemented isVowel method

--- WordPlay.java
+++ WordPlay.java
@@ -6,5 +6,26 @@
  * @version (a version number or a date)
  */
 public class WordPlay {
+    
+    public boolean isVowel(char ch) {
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+        for (char v : vowels) {
+            if (Character.toLowerCase(ch) == v)
+                return true;    
+        }
+        return false;
+    }
+    
+    public void testIsVowel() {
+        System.out.println(isVowel('F'));
+        System.out.println(isVowel('f'));
+        System.out.println(isVowel('a'));
+        System.out.println(isVowel('A'));
+    }
+    
+    public static void main(String[] args) {
+        WordPlay wp = new WordPlay();
+        wp.testIsVowel();
+    }
 
 }
