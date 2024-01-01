96f4f7ce58fedd6e31efae7932200c9c9aef6bc0
Vaishali Vyas <sayvilahsiav@outlook.com>
2024-01-01 18:33:16 +0530

    Implemented replaceVowels method

--- WordPlay.java
+++ WordPlay.java
@@ -16,6 +16,17 @@ public class WordPlay {
         return false;
     }
     
+    public String replaceVowels(String phrase, char ch) {
+        StringBuilder replaced = new StringBuilder();
+        for (int i = 0; i < phrase.length(); i++) {
+            if (isVowel(phrase.charAt(i))) {
+                replaced.append(ch);
+            }
+            else replaced.append(phrase.charAt(i));
+        }
+        return replaced.toString();
+    }
+    
     public void testIsVowel() {
         System.out.println(isVowel('F'));
         System.out.println(isVowel('f'));
@@ -23,9 +34,14 @@ public class WordPlay {
         System.out.println(isVowel('A'));
     }
     
+    public void testReplaceVowels() {
+        System.out.println(replaceVowels("Hello World", '*'));
+        System.out.println(replaceVowels("Hello World", 'y'));
+    }
+    
     public static void main(String[] args) {
         WordPlay wp = new WordPlay();
-        wp.testIsVowel();
+        wp.testReplaceVowels();
     }
 
 }
